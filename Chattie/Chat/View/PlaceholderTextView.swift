0a89ef2aefed22cb168790ce75a8e4cfdbe68e53
niemczus <niemczus@gmail.com>
2022-03-14 17:25:37 +0100

    Placeholder Text View added

---
+++ Chattie/Chat/View/PlaceholderTextView.swift
@@ -0,0 +1,37 @@
+//
+//  PlaceholderTextView.swift
+//  Chattie
+//
+//  Created by Kamil Niemczyk on 14/03/2022.
+//
+
+import UIKit
+
+class PlaceholderTextView: UITextView {
+    
+    let placeholderLabel = UILabel()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        setupView()
+    }
+    
+    func setupView() {
+        delegate = self
+        
+        placeholderLabel.frame = CGRect(x: 7, y: -8, width: bounds.width, height: bounds.height)
+        placeholderLabel.textColor = .lightGray
+        placeholderLabel.font = font
+        
+        placeholderLabel.text = "Enter a message"
+        
+        addSubview(placeholderLabel)
+    }
+}
+
+extension PlaceholderTextView: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        placeholderLabel.isHidden = textView.text.count != 0
+    }
+}
